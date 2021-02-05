4b857514ed0a3699c812cc4e13113db78b233c83
Nilauthaya <nilauthaya98@.com>
2021-02-05 18:01:39 +0530

    car

--- src/app/profileform/profileform.component.ts
+++ src/app/profileform/profileform.component.ts
@@ -1,6 +1,6 @@
 // import { variable } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,FormBuilder,Validators} from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -12,32 +12,59 @@ import { FormGroup, FormControl,FormBuilder,Validators} from '@angular/forms';
 export class ProfileformComponent implements OnInit {
 
   profileForm!: FormGroup;
-  obj:any = {};
+  obj: any = {};
+  array: any = []
+  arr :any = []
+   detail:any;
   sectionSubmitted = false;
-  buttonText:string = "";
+  buttonText: string = "";
   constructor(
-    private formBuilder: FormBuilder ,
-     ) { 
-     }
-    
+    private formBuilder: FormBuilder,
+  ) {
+  }
+
   ngOnInit(): void {
     this.profileForm = this.formBuilder.group({
       name: [null, Validators.required],
-      modal:[null,Validators.required],
-      price:[null,Validators.required],
-      color:[null,Validators.required],
-      wheels:[null,Validators.required]
+      modal: [null, Validators.required],
+      price: [null, Validators.required],
+      color: [null, Validators.required],
+      wheels: [null, Validators.required]
     })
-    console.log(this.obj)
+    this.detail = localStorage.getItem("array");
+this.arr = JSON.parse(this.detail)
   }
   get f() { return this.profileForm.controls };
 
-  addterm(){
-    this.sectionSubmitted = true;
-    if(this.profileForm.invalid){
+  addterm() {
+    this.sectionSubmitted = false;
+    if (this.profileForm.invalid) {
       return;
-     
     }
+ 
+  
+    if(localStorage.getItem('array') === null ){
+      this.array = [];
+    }
+    else{
+      this.detail = localStorage.getItem("array");
+      this.array = JSON.parse(this.detail)
+    }    
+    this.array.push(this.obj)
+    localStorage.setItem('array', JSON.stringify(this.array));
+    this.detail = localStorage.getItem("array");
+
+    this.array = JSON.parse(this.detail)
+    
+  
+    
+    console.log(this.array)
+    this.ngOnInit()
   }
+
+
+
+   
+  
 
 }
